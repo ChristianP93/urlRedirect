ec8fb9f58557f91cfce39da06bddc2c0425cee7c
Republic <christian.pengu@ad2014.it>
2017-01-27 15:06:18 +0100

    init2

--- test/10-url.js
+++ test/10-url.js
@@ -22,26 +22,3 @@ it('should post new url [POST /api/v1/url/link]', (done)=>{
       console.log(res);
     })
 })
-
-// it('should get all the categories [POST /v1/categories]', function(done) {
-//     request
-//       .get(testUtils.url + '/v1/categories')
-//       .set('Authorization', 'Bearer ' + oauthToken)
-//       .on('error', done)
-//       .end(function(res){
-//             log.debug(res.body.categories);
-//             res.statusCode.should.equal(200); // OK
-//             should.exist(res.body.categories);
-//             res.body.categories.should.be.an('array');
-//             //res.body.categories.should.have.length(3);
-//             res.body.categories.should.have.length.of.at.least(3);
-//             res.body.categories.should.all.have.a.property('_id');
-//             res.body.categories.should.all.have.a.property('title');
-//             res.body.categories.should.all.have.a.property('icon');
-//             res.body.categories.should.all.not.have.a.property('tags');
-//             for(var i=0; i<res.body.categories; i++){
-//               res.body.categories[i].sorting.shoould.equal(i);
-//             }
-//             done();
-//       });
-//   });
